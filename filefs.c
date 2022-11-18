88f71496facf5f09d698d4f7421fcd0590c38433
Blake Rhodes <blakester2019@gmail.com>
2022-11-18 16:40:13 -0600

    Remove works. List works. Need to populate blocks in data

--- filefs.c
+++ filefs.c
@@ -74,6 +74,16 @@ int fileExists(char* filename);
 void insertFileIntoDirs(char* fname, int fbl_index, int inode_index, int prevInode);
 void updateInode(int inode_index, int IsDir, int size, int fbl_index);
 void updateFileSystem(char* fsname);
+int searchDirs(char* arr[], int counter);
+void extractInodeBlocks(int inode_id);
+int checkIfDirExists(char* fname, int parent_inode);
+void printFile(char* fname, int depth);
+void listFiles(int parent, int depth);
+void resetInode(int inode_id);
+
+// Dev Prototypes
+void printDirs();
+void printFBL(int start, int end);
 
 // ---- MAIN ----
 int main(int argc, char** argv)
@@ -124,20 +134,17 @@ int main(int argc, char** argv)
     exitusage(argv[0]);
 
   // Open the file
-  if ((fd = open(fsname, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1)
-  {
+  if ((fd = open(fsname, O_CREAT | O_RDWR, S_IRUSR | S_IWUSR)) == -1) {
     perror("open failed");
     exit(EXIT_FAILURE);
   }
-  else
-  {
+  else {
     // Check if it is a new file or not
     if (zerosize(fd))
       newfs = 1;
     
     // Create the 10MB fs file if it is new
-    if (newfs)
-    {
+    if (newfs) {
       formatNewFS(fsname);
       close(fd);
     }
@@ -153,37 +160,44 @@ int main(int argc, char** argv)
     char* token;
     char* tokenArray[50];
     
-    for (int i = 0; i < 50; i++)
-    {
+    for (int i = 0; i < 50; i++) {
       tokenArray[i] = malloc(255);
       strcpy(tokenArray[i], "NULL");
     }
     
     int counter = 0;
     token = strtok(toadd, "/\\");
-    while (token != NULL)
-    {
+    while (token != NULL) {
       strcpy(tokenArray[counter], token);
       counter++;
       token = strtok(NULL, "/\\");
     }
 
-    printf("File: %s\n", tokenArray[counter - 1]);
-
     // Insert Directories
-    int prevInode = -1;
-    for (int i = 0; i < counter - 1; i++)
-    {
+    int prevInode = 0;
+    int next;
+    for (int i = 0; i < counter - 1; i++) {
+      if (next = checkIfDirExists(tokenArray[i], prevInode))
+      {
+        prevInode = next;
+        continue;
+      }
       int fbl_index = findFBLIndex(0, 150);
       int inode_index = findInodeIndex();
       insertFileIntoDirs(tokenArray[i], fbl_index, inode_index, prevInode);
       updateInode(inode_index, 1, 0, fbl_index);
       prevInode = inode_index;
     }
+
+    // Check if file already exists
+    next = checkIfDirExists(tokenArray[counter - 1], prevInode);
+    if (next) {
+      printf("This file already exists in this directory\n");
+      exit(-1);
+    }
     
     // Get Stats for file
-    if (!fileExists(tokenArray[counter - 1]))
-    {
+    if (!fileExists(tokenArray[counter - 1])) {
       printf("No file named %s\n", tokenArray[counter - 1]);
       exit(-1);
     }
@@ -199,54 +213,33 @@ int main(int argc, char** argv)
     insertFileIntoDirs(tokenArray[counter - 1], fbl_index, inode_index, prevInode);
     updateInode(inode_index, 0, size, fbl_index);
 
-    // TODO: Insert data into blocks
+    // Insert data into blocks
     int numBlocks = (size + (512 - 1)) / 512;
     int block_ids[numBlocks];
-    for (int i = 0; i < numBlocks; i++)
-    {
+    for (int i = 0; i < numBlocks; i++) {
       block_ids[i] = findFBLIndex(150, 10150);
-      inodes[inode_index].blocks[i] = block_ids[i];
+      inodes[inode_index].blocks[i + 1] = block_ids[i];
     }
 
+    // MAXS PROBLEM FROM HERE
     FILE *fp = fopen(tokenArray[counter - 1], "r");
     int remaining = size;
-    for (int i = 0; i < numBlocks; i++)
-    {
+    for (int i = 0; i < numBlocks; i++) {
       char buffer[512];
-      if (remaining >= 511)
-      {
-        fread(buffer, 511, 1, fp);
+      if (remaining >= 511) {
+        strcpy(buffer, "");
+        fread(&buffer, 1, 511, fp);
         remaining = remaining - 511;
       }
-      else
-      {
-        fread(buffer, remaining, 1, fp);
+      else {
+        strcpy(buffer, "");
+        fread(&buffer, 1, 1, fp);
+        printf("%s\n", buffer);
         remaining = 0;
       }
       strcpy(data.blocks[block_ids[i]].data, buffer);
     }
-    printf("Data Block\n");
-    for (int i = 0; i < numBlocks; i++)
-    {
-      printf("STARTING BLOCK %d\n", block_ids[i]);
-      printf("%s\n", data.blocks[block_ids[i]].data);
-    }
-
-    
-
-    // Printing stuff
-    for (int i = 0; i < 150; i++)
-    {
-      if (fbl.blocks[i] == 1)
-      {
-        File testDir = dirs.files[i];
-        printf("Some File or Directory: %s\n", testDir.name);
-      }
-    }
-
-    printf("Blocks for file:\n");
-    for (int i = 0; i < numBlocks; i++)
-      printf("%d\n", block_ids[i]);
+    // DOWN TO HERE
     
     // Truncate fs file and write SB, FBL, inodes, and Data
     updateFileSystem(fsname);
@@ -258,13 +251,35 @@ int main(int argc, char** argv)
 
   if (remove)
   {
-    // Find directory containing toremove
-    // Find associated inode (do not remove yet)
-    // Clear data blocks and flip the blocks to free in the FBL
-    // Set inode to not in use
+    // Tokenize path
+    char* token;
+    char* tokenArray[50];
+    
+    for (int i = 0; i < 50; i++) {
+      tokenArray[i] = malloc(255);
+      strcpy(tokenArray[i], "NULL");
+    }
+    
+    int counter = 0;
+    token = strtok(toremove, "/\\");
+    while (token != NULL) {
+      strcpy(tokenArray[counter], token);
+      counter++;
+      token = strtok(NULL, "/\\");
+    }
+
+    // Determine if file is in the path
+    int file_inode = searchDirs(tokenArray, counter);
+    
+    if (file_inode <= 0) {
+      printf("File could not be found\n");
+      exit(-1);
+    }
+
+    resetInode(file_inode);
     // Also keep in mind we need to remove any directories which do not
     // contain a "regular" file (I think we need to do this)
-    // Truncate fs file and write SB, FBL, inodes, and data
+    updateFileSystem(fsname);
   }
 
   if (extract)
@@ -275,38 +290,32 @@ int main(int argc, char** argv)
     char* token;
     char* tokenArray[50];
     
-    for (int i = 0; i < 50; i++)
-    {
+    for (int i = 0; i < 50; i++) {
       tokenArray[i] = malloc(255);
       strcpy(tokenArray[i], "NULL");
     }
     
     int counter = 0;
     token = strtok(toextract, "/\\");
-    while (token != NULL)
-    {
+    while (token != NULL) {
       strcpy(tokenArray[counter], token);
       counter++;
       token = strtok(NULL, "/\\");
     }
     
-    for (int i = 0; i < counter - 1; i++)
-    {
-      for (int j = 0; j < 100; j++)
-      {
-        File temp = dirs.files[j];
-        if (strcmp(temp.name, tokenArray[i]) == 0)
-        {
-          printf("Found\n");
-        }
-      }
-    }
+    int file_inode = searchDirs(tokenArray, counter);
     
+    if (file_inode <= 0) {
+      printf("File could not be found\n");
+      exit(-1);
+    }
+
+    extractInodeBlocks(file_inode);
   }
 
   if(list)
   {
-    // Confused how to attatch file names to inodes. 
+    listFiles(0, 0);
   }
 
   unmapfs();
@@ -340,8 +349,7 @@ void formatNewFS(char* fsname)
 
   FILE* fp = fopen(fsname, "a");
 
-  if (!fp)
-  {
+  if (!fp) {
     printf("Could not open file\n");
     exit(-1);
   }
@@ -349,12 +357,23 @@ void formatNewFS(char* fsname)
   for (int i = 0; i < 10000; i++)
     t_fbl.blocks[i] = 0;
   
-  for (int i = 0; i < 100; i++)
-  {
+  for (int i = 0; i < 100; i++) {
     t_inodes[i].InUse = 0;
     for (int j = 0; j < 100; j++)
       t_inodes[i].blocks[j] = -1;
   }
+
+  // Create Root Directory
+  File root;
+  strcpy(root.name, "root");
+  root.inode_id = 0;
+  root.parent_inode = -1;
+  t_dirs.files[0] = root;
+  t_inodes[0].InUse = 1;
+  t_inodes[0].IsDir = 1;
+  t_inodes[0].size = 0;
+  t_fbl.blocks[0] = 1;
+  t_inodes[0].blocks[0] = 0;
   
   fwrite(&t_SB, sizeof(SuperBlock), 1, fp);
   fwrite(&t_fbl, sizeof(t_fbl), 1, fp);
@@ -372,8 +391,7 @@ void loadFileSystem(char* fsname)
 {
   FILE* fp = fopen(fsname, "r");
 
-  if (!fp)
-  {
+  if (!fp) {
     printf("File could not be opened for reading\n");
     exit(-1);
   }
@@ -388,11 +406,8 @@ void loadFileSystem(char* fsname)
 
 int findFBLIndex(int start, int end)
 {
-  for (int i = start; i < end; i++)
-  {
-    if (fbl.blocks[i] == 0)
-    {
-      printf("Flipped Block at %d\n", i);
+  for (int i = start; i < end; i++) {
+    if (fbl.blocks[i] == 0) {
       fbl.blocks[i] = 1;
       return i;
     }
@@ -402,10 +417,8 @@ int findFBLIndex(int start, int end)
 
 int findInodeIndex()
 {
-  for (int i = 0; i < 100; i++)
-  {
-    if (inodes[i].InUse == 0)
-    {
+  for (int i = 0; i < 100; i++) {
+    if (inodes[i].InUse == 0) {
       inodes[i].InUse = 1;
       return i;
     }
@@ -415,8 +428,7 @@ int findInodeIndex()
 int fileExists(char* filename)
 {
   FILE* fp = fopen(filename, "r");
-  if (fp)
-  {
+  if (fp) {
     fclose(fp);
     return 1;
   }
@@ -437,16 +449,14 @@ void updateInode(int inode_index, int IsDir, int size, int fbl_index)
 {
   inodes[inode_index].IsDir = IsDir;
   inodes[inode_index].size = size;
-  if (IsDir)
-    inodes[inode_index].blocks[0] = fbl_index;
+  inodes[inode_index].blocks[0] = fbl_index;
 }
 
 void updateFileSystem(char* fsname)
 {
   FILE* fp = fopen("temp", "a");
 
-  if (!fp)
-  {
+  if (!fp) {
     printf("Could not open file\n");
     exit(-1);
   }
@@ -460,4 +470,125 @@ void updateFileSystem(char* fsname)
 
   remove(fsname);
   rename("temp", fsname);
+}
+
+int searchDirs(char* arr[], int counter)
+{
+  int parent_inode = 0;
+  int next;
+  // File lives in the root
+  if (counter == 1) {
+    return checkIfDirExists(arr[0], 0);
+  }
+
+  // Search file path
+  for (int i = 0; i < counter - 1; i++) {
+    for (int j = 0; j < 150; j++) {
+      if (next = checkIfDirExists(arr[i], parent_inode)) {
+        parent_inode = next;
+        break;
+      }
+      else {
+        return -1;
+      }
+    }
+  }
+
+  return checkIfDirExists(arr[counter - 1], parent_inode);
+}
+
+// MAXS PROBLEM
+// Should iterate through the blocks of an inode and print the contents of the blocks in "data"
+void extractInodeBlocks(int inode_id)
+{
+  char buffer[inodes[inode_id].size + 512];
+  strcpy(buffer, "");
+  char blockBuffer[512];
+  int i = 0;
+  int currBlock = inodes[inode_id].blocks[i];
+  while (currBlock != -1)
+  {
+    strcpy(blockBuffer, data.blocks[currBlock].data);
+    strcat(buffer, blockBuffer);
+    i++;
+    currBlock = inodes[inode_id].blocks[i];
+  }
+  puts(buffer);
+}
+
+// Checks if a directory exists. If so, returns that directories inode_id
+int checkIfDirExists(char* fname, int parent_inode)
+{
+  for (int i = 0; i < 150; i++) {
+    if (dirs.files[i].parent_inode == parent_inode && strcmp(dirs.files[i].name, fname) == 0) {
+      return dirs.files[i].inode_id;
+    }
+  }
+  return 0;
+}
+
+// Used for debugging. Prints all directories, both regular and dir type
+void printDirs()
+{
+  for (int i = 0; i < 150; i++)
+  {
+    printf("-----------------\n");
+    printf("Dirs[%d]: %s\n", i, dirs.files[i].name);
+    printf("Inode: %d\n", dirs.files[i].inode_id);
+    printf("Parent: %d\n", dirs.files[i].parent_inode);
+  }
+}
+
+// Used for debugging. Prints fbl binary from a given start to given end
+void printFBL(int start, int end)
+{
+  for (int i = start; i < end; i++) {
+    printf("FBL[%d]: %d\n", i, fbl.blocks[i]);
+  }
+}
+
+// Helps the listFiles function. Prints tabs for depth and filename
+void printFile(char* fname, int depth)
+{
+  for (int i = 0; i < depth; i++) {
+    printf("  ");
+  }
+  printf(" %s\n", fname);
+}
+
+// List files will recursively parse the files in the fs
+void listFiles(int parent, int depth)
+{
+  for (int i = 1; i < 150; i++)
+  {
+    if (dirs.files[i].inode_id == 0) {
+      continue;
+    }
+
+    if (dirs.files[i].parent_inode == parent) {
+      printFile(dirs.files[i].name, depth);
+      listFiles(dirs.files[i].inode_id, depth + 1);
+    }
+  }
+}
+
+// Used in remove function to return an inode to its original state
+void resetInode(int inode_id)
+{
+  for (int i = 0; i < 100; i++) {
+    if (inodes[inode_id].blocks[i] != -1) {
+      int block_index = inodes[inode_id].blocks[i];
+      if (block_index < 150) {
+        strcpy(dirs.files[block_index].name, "");
+        dirs.files[block_index].inode_id = 0;
+        dirs.files[block_index].parent_inode = 0;
+        fbl.blocks[block_index] = 0;
+        inodes[inode_id].blocks[0] = -1;
+      } else {
+        strcpy(data.blocks[block_index].data, "");
+        inodes[inode_id].blocks[i] = -1;
+      }
+    }
+  }
+  inodes[inode_id].InUse = 0;
 }
